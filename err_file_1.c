47cba3d863681fe28a9e150228efb8b727ba449d
GriffinMulei <muleigriffin@gmail.com>
2023-07-25 02:58:24 -0700

    monty

---
+++ err_file_1.c
@@ -0,0 +1,76 @@
+#include "monty.h"
+#include <stdio.h>
+
+int malloc_error(void);
+int usage_error(void);
+int f_open_error(char *filename);
+int unknown_op_error(char *opcode, unsigned int line_number);
+int no_int_error(unsigned int line_number);
+
+
+/**
+ * malloc_error - Prints error messages for malloc.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int malloc_error(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * usage_error - Prints error messages for usage.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int usage_error(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * f_open_error - Prints file opening error messages.
+ * @filename: Name of file failed to open
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int f_open_error(char *filename)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * unknown_op_error - Prints unknown error messages.
+ * @opcode: where error occurred.
+ * @line_number: Monty bytecodes file where error occured.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int unknown_op_error(char *opcode, unsigned int line_number)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n",
+		line_number, opcode);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * no_int_error - Prints invalid monty_push argument error messages.
+ * @line_number: Monty bytecodes file where error occurred.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
+int no_int_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	return (EXIT_FAILURE);
+}
+
+
+/*
+ * File: err_file_1.c
+ * Auth: Hamza Abdullahi
+ *       Abel Udoh
+ */
